3069ba18e7d2e0902a1b2513c6695fe2826efba8
feylon <jamshid14092002@gmail.com>
2024-10-15 09:24:54 +0500

    NOmer animatsiyasi tayyor

--- Frontend/src/components/Circular progress.vue
+++ Frontend/src/components/Circular progress.vue
@@ -9,19 +9,38 @@
         />
         <path
           class="circle"
+          :class = "color"
           :stroke-dasharray="percentage + ', 100'"
           d="M18 2.0845
             a 15.9155 15.9155 0 0 1 0 31.831
             a 15.9155 15.9155 0 0 1 0 -31.831"
         />
-        <text x="18" y="20.35" class="percentage">{{ tasksDone }}/{{ totalTasks }}</text>
-        <text x="18" y="25.5" class="label text-white">Bajarildi</text>
+        <text x="18" y="20.35" class="percentage">
+        
+
+    <n-number-animation
+      ref="numberAnimationInstRef"
+      :from="0"
+      show-separator
+
+      :to="tasksDone"
+      :active="true"
+      :precision="0"
+    />/{{ totalTasks }}
+  
+        </text>
+        <text x="18" y="25.5" class="label1 text-white">{{percentage.toFixed(1)}} %</text>
+        <text x="18" y="29.5" class="label text-white">Bajarildi</text>
+
       </svg>
     </div>
   </template>
   
   <script setup>
-  import { computed, toRefs } from 'vue'
+  import { computed, toRefs, ref } from 'vue'
+  
+  const  color = ref('');
+  const numberAnimationInstRef = ref(null);
   
   const props = defineProps({
     tasksDone: {
@@ -34,10 +53,22 @@
     }
   })
   
-  // Calculate the percentage of completion
   const { tasksDone, totalTasks } = toRefs(props);
   const percentage = computed(() => (tasksDone.value / totalTasks.value) * 100);
-  </script>
+if (percentage.value >= 0 && percentage.value < 20) color.value = "danger"; 
+if (percentage.value >= 20 && percentage.value < 60) color.value = "yellow"; 
+if (percentage.value >= 60 && percentage.value < 80) color.value = "blue"; 
+if (percentage.value >= 80 && percentage.value <= 100) color.value = "green"; 
+
+
+
+const timer = setTimeout(() => {
+  
+  numberAnimationInstRef.value?.play();
+const numberAnimationInstRef = ref(null); 
+}, 500);
+clearTimeout(timer)
+</script>
   
   <style scoped>
   .circular-progress {
@@ -58,10 +89,31 @@
     stroke-width: 3.8;
   }
   
+  .blue{
+
+    stroke: #0c88c2;
+  }
+
+
+
+  .danger{
+    stroke: #eb400c;
+
+  }
+
+.yellow {
+  stroke: #8ea705;
+
+}
+
+.green{
+  stroke: #099447;
+  
+}
+
   .circle {
     fill: none;
     stroke-width: 2.8;
-    stroke: #0c88c2;
     stroke-linecap: round;
     animation: progress 1s ease-out forwards;
   }
@@ -74,7 +126,13 @@
   
   .label {
     fill: #696767;
-    font-size: 0.3em;
+    font-size: 0.2em;
+    text-anchor: middle;
+  }
+
+  .label1 {
+    fill: #e0d8d8;
+    font-size: 0.2em;
     text-anchor: middle;
   }
   
